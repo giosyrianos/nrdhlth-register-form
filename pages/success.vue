d940f6511bf661e3ce6835437ba820c843339ed8
giosyrianos <giosyrianos@gmail.com>
2025-02-05 13:43:29 +0200

    💅 fix ui in success page

--- pages/success.vue
+++ pages/success.vue
@@ -1,13 +1,16 @@
 <template>
-	<div>
-		<h1>This is success page</h1>
+	<div class="n-success-container n-grid-1 n-padding-xxl n-margin-auto ">
+		<provet-stack justify-content="center" align-items="center">
+		<h1 class="n-typescale-xxl">🎉</h1>
+		<h2 class="n-typescale-xl">Registration Successful</h2>
+		<p class="n-typescale-l">Thank you for registering!</p>
+		<nuxt-link to="/">Go back to Home</nuxt-link>
+		</provet-stack>
 	</div>
 </template>
 
 <script setup lang="ts">
-
+// For a basic success page, there's usually no logic needed.
+// But we keep this script block with TS in case we want to add future functionality.
 </script>
 
-<style scoped>
-
-</style>
